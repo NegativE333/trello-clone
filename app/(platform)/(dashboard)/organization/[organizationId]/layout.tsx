2f4ce4bc5dd25df54661eafb2dfcf5c46cfb1edb
Om Tekade <omtekade914@gmail.com>
2023-12-14 10:46:44 +0530

    error fix

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,26 +1,27 @@
-import { auth } from "@clerk/nextjs";
-import { OrgControl } from "./_components/org-control";
 import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
-export async function generateMetaData() {
-    const { orgSlug } = auth();
+import { OrgControl } from "./_components/org-control";
 
-    return{
-        title: startCase(orgSlug || "organization"),
-    }
-}
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+};
 
 const OrganizationIdLayout = ({
-    children
-} : {
-    children: React.ReactNode
+  children
+}: {
+  children: React.ReactNode;
 }) => {
-    return (  
-        <>
-            <OrgControl />
-            {children}
-        </>
-    );
-}
- 
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
 export default OrganizationIdLayout;
